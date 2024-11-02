ebc8f7e681da4dd343eb0da95833e5e37869061a
moliinyk <maks.oliynik228@gmail.com>
2024-11-02 13:11:35 +0200

    Updated project structure

--- app/src/App.js
+++ app/src/App.js
@@ -1,28 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-  const likes = 50;
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Liked {likes} times</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>...</div>;
 }
 
 export default App;
